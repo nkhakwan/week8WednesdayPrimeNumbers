00d99f96989a8c9bfba51f4d7518f721bcead40d
nkhakwan <post_khan@yahoo.com>
2020-05-06 10:20:29 -0700

    committing after formatting the code in class

--- Prime.cs
+++ Prime.cs
@@ -1,32 +1,41 @@
+using System;
+using System.Collections.Generic;
 
 
-
-
-int GivenNumber = 100;
-List<int> ReducingList = new List<int>(0);
-for (int i=2; i<GivenNumber; i=i+1)
+namespace PrimeNumbers
 {
-  ReducingList.Add(i);
-}
+  public class Program
+  {
 
-for (int i=2; i<GivenNumber; i=i+1)
-{
-  for (int j=i+1; j<GivenNumber; j=i+1)
-  {
-    if ((ReducingList(j)%i))
+    public static void Main()
     {
-      ReducingList[j] = 0;
-    }
-    
-  }
-  for(int l=0; l<ReducingList.length; l=l+1)
-  {
-    if (ReducingList[l] == 0)
-    {
-      ReducingList.Remove[l];
+      int GivenNumber = 100;
+      List<int> ReducingList = new List<int>(0);
+      for (int i=2; i<GivenNumber; i=i+1)
+      {
+        ReducingList.Add(i);
+      }
+
+      for (int i=2; i<GivenNumber; i=i+1)
+      {
+        for (int j=i+1; j<GivenNumber; j=i+1)
+        {
+          if ((ReducingList(j)%i))
+          {
+            ReducingList[j] = 0;
+          }
+          
+        }
+        for(int l=0; l<ReducingList.length; l=l+1)
+        {
+          if (ReducingList[l] == 0)
+          {
+            ReducingList.Remove[l];
+          }
+        }
+      }
     }
   }
 }
-return ReducingList;
 
 
