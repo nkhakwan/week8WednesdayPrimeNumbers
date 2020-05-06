1abcc5c87babbdc428fba6de455e0e29ffec4262
nkhakwan <post_khan@yahoo.com>
2020-05-06 13:23:41 -0700

    committing file that has half of the code running ok. It finalizes prime numbers but still has to take 0' out

---
+++ Personal.csn
@@ -0,0 +1,37 @@
+namespace  Practice.Make
+{
+  public class CarSpecs
+  {
+    private string _carMake;
+    private int _mileage;
+    private int _price;
+  
+
+  public CarSpecs(string carMake, int mileage, int price)
+  {
+    _carMake = carMake;
+    _mileage = mileage;
+    _price = price;
+  }
+
+  public string GetCarMake()
+  {
+    return _carMake;
+  }
+
+  public int GetPrice()
+  {
+    return _price;
+  }
+
+  public int GetMileage()
+  {
+    return _mileage;
+  }
+
+  public bool UnderBudget(int Budget)
+  {
+    return (_price<Budget);
+  }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace PrimeNumbers
+{
+  public class Program
+  {
+
+    public static void Main()
+    {
+      Console.WriteLine("Please input a number that you want to find all the primes below that number");
+      string InputNumber = Console.ReadLine();
+      int GivenNumber = int.Parse(InputNumber);
+      List<int> ReducingList = new List<int>(0);
+      for (int i=1; i<GivenNumber+1; i=i+1)
+      {
+        ReducingList.Add(i);
+        //Console.WriteLine(i);
+        
+      }
+      //Console.WriteLine(ReducingList);
+     /*foreach(int Prime in ReducingList)
+      {
+       Console.WriteLine(Prime);
+      }*/
+
+
+
+      for (int i=1; i<ReducingList.Count; i=i+1)
+      {
+        for (int j=i+1; j<ReducingList.Count; j=j+1)
+        {
+          if ((ReducingList[j]%ReducingList[i])==0)
+          {
+            ReducingList[j] = 0;
+          }
+          Console.WriteLine(j + " " +ReducingList[j]);
+        }
+        Console.WriteLine("outofsmallloop");
+      }
+        for(int l=0; l<ReducingList.Count; l=l+1)
+        {
+          if (ReducingList[l] == 0)
+          {
+            ReducingList.Remove(ReducingList[l]);
+          }
+        }
+      
+      foreach(int Prime in ReducingList)
+      {
+        Console.WriteLine(Prime);
+      }
+    }
+  }
+}
+
+
