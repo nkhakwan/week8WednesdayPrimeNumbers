fe03dadc3036615c2b643b407b335e8977a120e3
nkhakwan <post_khan@yahoo.com>
2020-05-06 17:13:24 -0700

    committing unfinished file and would update tomorrow

---
+++ Dictionary.cs
@@ -0,0 +1,6 @@
+Dictionary<int, string> NewVenture = new Dicitionary<int, string>();
+
+NewVenture.Add(1, "suzuki");
+NewVenture.Add(2, "Santro");
+NewVenture.Add(3, "Geo Prism");
+NewVenture.Add(4, "Honda Accor");
